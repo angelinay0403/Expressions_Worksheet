32b20fd2cfeb7433a3a59bd519c6e640314a6144
angelinay0403 <angelinay0403@gmail.com>
2013-11-07 16:52:40 -0500

    Expression worksheet unfinished

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//Angelina Castro, 11/7/13 Expression Worksheet assigment.
+
+
+// calculate sparky age in dog years.
+var humanYear = 8;
+var dogYear = 7;
+var sparkyAge = humanYear * dogYear;
+Console.log(sparkyAge);
+
+
+
